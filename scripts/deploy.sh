a2b32afdd57a3744ad8620cdce8028a8dca54a68
Fabio <cerri194@hotmail.com>
2022-03-10 12:34:34 +0100

    Fixing path

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,9 +2,8 @@
 ARTIFACTS=/mnt/common/artifacts
 WCONFIG=/work/config
 echo "Copying files to WAS configuration folders.."
-cp "$ARTIFACTS/*" "$WCONFIG"
+cp "$ARTIFACTS/"* "$WCONFIG"
 echo "Files copied!"
 echo "Executing configuration for WAS.."
 env JVM_EXTRA_CMD_ARGS=-Xnoloa /work/configure.sh
 echo "Configuration done!"
-
