6c9b1c0bc025c567ef5b032961717aca980fc371
Fabio <cerri194@hotmail.com>
2022-03-14 11:07:46 +0100

    Adding passowrd user to stop server

--- scripts/stop.sh
+++ scripts/stop.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 PROFILE_NAME=${PROFILE_NAME:-"AppSrv01"}
 SERVER_NAME=${SERVER_NAME:-"server1"}
+USR=wasadmin
+PSW=`cat/tmp/PASSWORD`
 
 echo "Stopping server ..................."
-/opt/IBM/WebSphere/AppServer/profiles/$PROFILE_NAME/bin/stoptServer.sh $SERVER_NAME
+/opt/IBM/WebSphere/AppServer/profiles/$PROFILE_NAME/bin/stoptServer.sh $SERVER_NAME  -username $USR -password $PSW
