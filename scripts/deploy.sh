c1f16976f0526571ad2450fe79cab06f82143445
Fabio <cerri194@hotmail.com>
2022-03-14 09:52:03 +0100

    Adding stop server

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,7 +6,7 @@ echo "Copying files to WAS configuration folders.."
 cp "$ARTIFACTS/"* "$WCONFIG"
 echo "Files copied!"
 echo "Executing configuration for WAS.."
-env JVM_EXTRA_CMD_ARGS=-Xnoloa "$WCONFIG"/configure.sh "$@"
+env JVM_EXTRA_CMD_ARGS=-Xnoloa "$SCRIPTS"/configure.sh "$@"
 echo "Configuration done!"
 
 
