4151cdca9ae5c6502934f951782d27881bf60dd5
Fabio <cerri194@hotmail.com>
2022-03-14 13:52:35 +0100

    Adding pasword as prereq to start

--- scripts/start.sh
+++ scripts/start.sh
@@ -1,7 +1,11 @@
 PROFILE_NAME=${PROFILE_NAME:-"AppSrv01"}
 SERVER_NAME=${SERVER_NAME:-"server1"}
 USR=wsadmin
+
+echo 'setting password'
+/work/set_password.sh
+
 PSW=`cat /tmp/PASSWORD`
 
-echo "Stopping server ..................."
+echo "Starting server ..................."
 /opt/IBM/WebSphere/AppServer/profiles/$PROFILE_NAME/bin/startServer.sh $SERVER_NAME  -username $USR -password $PSW -timeout 300
