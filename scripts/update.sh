6c73bc291c04a8acb3ce7060c153d80ef2797e7f
Fabio <cerri194@hotmail.com>
2022-03-28 10:07:57 +0200

    Fix minor bugs

--- scripts/update.sh
+++ scripts/update.sh
@@ -2,7 +2,7 @@
 SCRIPTS="$CHE_PROJECTS_ROOT"/wasbase/scripts
 echo "Updating Websphere app.."
 # args[0]: The anme of the script to be executed
-/work/run_py_script.sh "@SCRIPTS"/update_app.py "$@"
+/work/run_py_script.sh "$SCRIPTS"/update_app.py "$@"
 echo "Update app done!"
 
 
